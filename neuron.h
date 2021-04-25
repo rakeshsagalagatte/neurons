07894d7029d5da2ead16d973b686d520013f9841
Rakesh S R <rakeshsagalagatte@gmail.com>
2021-04-25 13:49:38 +0530

    AND , OR and NOT neurons added

---
+++ neuron.h
@@ -0,0 +1,31 @@
+#ifndef NEURON_H_INCLUDED
+#define NEURON_H_INCLUDED
+
+int and_neuron(float *inputN, float weight, int arraySize){
+     int i=0;
+     float sum = 0.0;
+     for(i=0;i < arraySize ; i++){
+         sum = sum + inputN[i] * weight;
+     }
+     float threshold = weight * arraySize;
+     printf("%f , %f\n",sum, threshold );
+     return (sum >= threshold);
+}
+
+int or_neuron(float *inputN, float weight, int arraySize){
+    int i=0;
+    float sum =0.0;
+    for(i=0;i < arraySize; i++){
+        sum += inputN[i] * weight;
+    }
+    return (sum >= weight);
+}
+
+void not_neuron(float *inputN, float weight, int arraySize){
+    int i=0;
+    for(i=0; i < arraySize;i++){
+        inputN[i] = ((inputN[i] * weight) < weight);
+    }
+}
+
+#endif
